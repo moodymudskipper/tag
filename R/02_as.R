864de6b759c6af436a5f37d6dfda07c5c464c417
moodymudskipper <antoine.fabri@gmail.com>
2019-05-31 13:46:38 +0200

    improved doc

--- R/02_as.R
+++ R/02_as.R
@@ -1,42 +1,48 @@
-# tag_adverb is a clean definition that doesn't call any of tag, as_tag, as_tag_adverb
+# tag_adverb doesn't call any of tag, as_tag, as_tag_adverb
 # tag calls tag_adverb and as_tag on top of it
 # if as_tag receives a tag adverb with a tag_adverb(...) definition, it
 # adapts the definition by substituting,
 
-
 # When defining a tag or an adverb, they get a definition and a class
 # when converting, we check these definitions
-# if the definition starts with `tag` or `tag_adverb` we can do a substitution
+# If the definition starts with `tag` or `tag_adverb` we can do a substitution
 # and execute it, it will ensure that
-# as_tag(as_tag_adverb(x)) is the identity transofmation for a tag adverb
-# defined in this package (and same for tag adverbs)
-
+# as_tag(as_tag_adverb(x)) is the identity transformation for a tag adverb
+# defined in this package. The conversion works in a similar fashion in the
+# opposite direction.
 
 #' Convert to a tag or a tag_adverb
 #'
-#' Convert without loss of information
+#' tags can be converted to tag_adverbs and conversely, standard function
+#' operators can also be converted to tag or tag adverbs.
 #'
-#' @param f a function operator (adverb)
-#'
+#' @param f a function
 #' @export
 as_tag <- function(f){
   if(is_tag(f)) return(f)
+
+  # clean up enclosing environment on exit
   on.exit(rm(tag_, tag_formals))
 
-  # We only convert to tags from tag_adverbs
+  # Convert first to tag_adverb
   f <- as_tag_adverb(f)
+
+  # tags have same formals as tag_adverbs, minus the first one
   tag_formals <- formals2(f)[-1]
+
   tag_ <- as.function(c(tag_formals, quote({
-    # my_tag(t_arg = foo) needs to translate into partial(f, t_arg = foo)
+    # get args from call
     partial_args <- as.list(match.call())[-1]
+    # a tag creates a tag adverb, which is f partialized on given arguments
     tag_adv <- purrr::partial(f, !!!partial_args)
-    attr(tag_adv, "definition") <- rlang::expr(purrr::partial(!!f, !!!partial_args))
+    # give the tag adverb has a definition attribute and a class
+    attr(tag_adv, "definition") <- expr(purrr::partial(!!f, !!!partial_args))
     add_class(tag_adv, "tag_adverb")
   })))
-  tag_ <- add_class("tag", x = tag_)
 
-  # if we have a proper tag_adverb definition, we adapt it to have a tag definition
-  # if we have a tag_adverb definition, we adapt it likewise
+  # give the tag a definition attribute and a class
+
+  # tag definitions are adapted from tag_adverb definitions
   attr(tag_, "definition") <- attr(f, "definition")
   attr(tag_, "definition")[[1]] <-
   if(identical(attr(f, "definition")[[1]], quote(tag_adverb))) {
@@ -44,10 +50,9 @@ as_tag <- function(f){
   } else if(identical(attr(f, "definition")[[1]], quote(as_tag_adverb))) {
     quote(as_tag)
   } else {
-    rlang::expr(as_tag(!!attr(f, "definition")))
+    expr(as_tag(!!attr(f, "definition")))
   }
-  # attr(tag_, "definition") <- do.call(substitute, list(match.call(), list(f = eval(f))))
-  tag_
+  add_class("tag", x = tag_)
 }
 
 
@@ -66,7 +71,7 @@ as_tag_adverb<- function(f) {
     add_class(adv, "tag_adverb")
   } else  {
     # if it's an external adverb we rework it a bit and add a definition attribute
-    adv <- as.function(c(formals(f),rlang::expr({
+    adv <- as.function(c(formals(f),expr({
       f <- !!f
       f_fmls <- formals()
       # build manufactured function
