7157de2a8c2ccf19729a455295705bf388619676
moodymudskipper <antoine.fabri@gmail.com>
2019-06-22 17:55:34 +0200

    added test

--- tests/testthat/tests_tags_from_tags.R
+++ tests/testthat/tests_tags_from_tags.R
@@ -1,8 +1,20 @@
-library(tags)
+#library(tags)
 
 expect_no_error <- purrr::partial(expect_error, regexp = NA)
 
+
 test_that("logging tag works",{
+  logging <- tag(args = alist(.time = TRUE, .print = TRUE),{
+    message(deparse(sys.call()))
+    if(.time) {
+      time_sec <- system.time(res <- CALL(eval = TRUE))[3]
+      cat("  ~", time_sec, "sec\n")
+    } else {
+      res <- CALL(eval = TRUE)
+    }
+    if(.print) print(res)
+    invisible(res)
+  })
   capture.output({
     expect_equal(logging$identity("foo"),"foo")
     expect_message(logging$identity("foo"), 'logging$identity("foo")', fixed= TRUE)
@@ -30,3 +42,16 @@ test_that("logging tag works",{
 #   fun6 <- function(t1, e, s, f, ...) t1(.quiet=s)$f(..., .otherwise = e)
 #   expect_equal(fun6(trying,  "hi", TRUE , paste, "hello",world), "hi")
 # })
+
+test_that("wrapping tag works",{
+wrapping <- tag(
+  args= alist(.before=NULL, .after=NULL, .around = identity),
+  eval_args = FALSE,{
+    t_args <- T_ARGS(eval = FALSE)
+    eval(t_args[[".before"]])
+    . <- eval(expr(rlang::as_function(!!t_args[[".around"]])(!!CALL(eval = FALSE))))
+    eval(t_args[[".after"]])
+    .
+  })
+expect_no_error(wrapping(print("hi!"))$print('hello'))
+})
